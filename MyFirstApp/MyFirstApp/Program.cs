5b7fde572dfa29810514a35737284f662c57bea1
jakuborzelowski <s27977@pjwstk.edu.pl>
2024-03-13 23:54:11 +0100

    zadanie 4

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -19,3 +19,10 @@ static double GetAverage(int[] x)
 int[] x = new []{1,2,3,4};
 
 Console.WriteLine(GetAverage(x));
+
+static int GetMax(int[] x)
+{
+    return x.Max();
+}
+
+Console.WriteLine(GetMax(x));
